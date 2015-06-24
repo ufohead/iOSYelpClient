2a6e3acc3b00e086d56399f76361ff411ca2d69e
Ufohead Tseng <ufohead@yahoo-inc.com>
2015-06-24 23:08:21 +0800

    init

---
+++ Yelp/BusinessCell.h
@@ -0,0 +1,17 @@
+//
+//  BusinessCell.h
+//  Yelp
+//
+//  Created by Ufohead Tseng on 2015/6/24.
+//  Copyright (c) 2015年 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Business.h"
+
+
+@interface BusinessCell : UITableViewCell
+
+@property (nonatomic, strong) Business * business;
+
+@end
